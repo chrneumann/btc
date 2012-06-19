07f13489073de38f25fe7c54705e776baa61290f
Christian Neumann <cneumann@datenkarussell.de>
2012-06-19 17:40:19 +0200

    First commit

---
+++ tunes/afoxe.js
@@ -0,0 +1,21 @@
+var afoxe = { name: 'Afoxe',
+						 tune_sign: 'shaving_armpit',
+						 bars: 4,
+						 measure: 4,
+						 length: 2,
+						 breaks: 
+						 [ { name: 'Groove',
+								 instruments: 
+								 { surdo: { beat: [ 25, 29 ] },
+									 repinique: 
+									 { flare: [1, 9, 17],
+										 rim: [7, 15, 23, 27, 31],
+										 hand: [4, 12, 20],
+									 },
+									 snare:
+									 { beat: [],
+									   shadow: []},
+									 tamborim: { beat: [1, 3, 5, 7, 9, 10] },
+									 agogo: 
+									 { low: [5, 7],
+										 high: [1, 3] } } } ] };
